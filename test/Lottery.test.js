82ea40571389d73453e2014150524ee9ff3ed027
Zac <thomazac@mail.gvsu.edu>
2022-02-11 15:59:37 -0600

    add some more tests

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -37,4 +37,62 @@ describe('Lottery Contract', () => {
         assert.equal(1, players.length);
     });
 
-})
+    it('allows multiple account to enter', async () =>{
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether') // given in wei
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.02', 'ether') // given in wei
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.02', 'ether') // given in wei
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length);
+    });
+
+    it('requires a minimum amount of ether to enter', async () => {
+        try{
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 200 // in wei, will fail
+            });
+
+            // If error was not thrown, the test would fail
+            assert(false);
+
+        } catch (err){
+            // Error present
+            assert(err);
+        }
+    });
+
+    it('only manager can call pickWinner', async () => {
+        try{
+            // Not manager
+            await lottery.methods.pickWinner().send({
+                from: accounts[1]
+            });
+
+            // If error was not thrown, the test would fail
+            assert(false);
+
+        } catch (err){
+            // Error present
+            assert(err);
+        }
+    });
+
+});
